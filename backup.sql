7172ac74eef437e3a0390895df40f99e29c4461f
Lulich <eregin.new@gmail.com>
2022-06-12 18:11:13 +0300

    Adding new information

--- backup.sql
+++ backup.sql
@@ -8,7 +8,7 @@
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'WIN866';
+SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SELECT pg_catalog.set_config('search_path', '', false);
 SET check_function_bodies = false;
@@ -117,13 +117,16 @@ SET default_table_access_method = heap;
 CREATE TABLE public.deeperpagedogs (
     breed character varying(50) NOT NULL,
     photo text,
-    weight character varying(70),
-    height character varying(70),
     life_duration character varying(70),
     main_purpose character varying(50),
     full_description text,
     main_characteristics character varying(50)[],
-    size character varying(30)
+    size character varying(30),
+    weight_border1 integer,
+    weight_border2 integer,
+    height_female integer,
+    height_male integer,
+    CONSTRAINT deeperpagedogs_size_check CHECK (((size)::text = ANY ((ARRAY['¬Ґ«Є п'::character varying, 'агз­ п'::character varying, 'баҐ¤­пп'::character varying, 'ЄагЇ­ п'::character varying, '®зҐ­м ЄагЇ­ п'::character varying])::text[])))
 );
 
 
@@ -148,7 +151,8 @@ ALTER TABLE public.mainpagedogs OWNER TO postgres;
 
 CREATE TABLE public.photogallery (
     breed character varying(50),
-    photos character varying(100)[]
+    photos character varying(100)[],
+    CONSTRAINT photogallery_breed_check CHECK ((breed IS NOT NULL))
 );
 
 
@@ -179,7 +183,9 @@ CREATE TABLE public.shelterdogs (
     shelter_name character varying(30) NOT NULL,
     gender character varying(10) NOT NULL,
     full_description text,
-    weight character varying(30),
+    weight integer NOT NULL,
+    height integer NOT NULL,
+    CONSTRAINT shelterdogs_age_check CHECK ((age > 0)),
     CONSTRAINT shelterdogs_gender_check CHECK (((gender)::text = ANY ((ARRAY['male'::character varying, 'female'::character varying])::text[])))
 );
 
@@ -215,7 +221,9 @@ CREATE TABLE public.shelters (
     shelter_name character varying(30) NOT NULL,
     shelter_location character varying(50),
     phone_number character varying(15),
-    dogs_counter integer DEFAULT 0
+    dogs_counter integer DEFAULT 0,
+    CONSTRAINT shelters_phone_number_check CHECK ((phone_number IS NOT NULL)),
+    CONSTRAINT shelters_shelter_location_check CHECK ((shelter_location IS NOT NULL))
 );
 
 
@@ -246,10 +254,15 @@ ALTER TABLE ONLY public.shelterdogs ALTER COLUMN id SET DEFAULT nextval('public.
 -- Data for Name: deeperpagedogs; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.deeperpagedogs (breed, photo, weight, height, life_duration, main_purpose, full_description, main_characteristics, size) FROM stdin;
-ЂЁ¤Ё	https://catfishes.ru/wp-content/uploads/2020/09/atlas1.jpg	¤® 30 ЄЈ	¤® 62 б¬	®в 10 ¤® 12 «Ґв	Hunting	‘®Ў Є  Є« бб­ п п Ўл бҐЎҐ ў§п«. “ ¬Ґ­п в®«мЄ® ¤Ґ­ҐЈ ­Ґв,   в Є-в® ®Ја®¬­ п еҐа­п в Є п ЇгиЁвб п, еў®бв®¬ ўЁ«пҐв Ё « бЄ®ў п,­ ўҐа­®Ґ, Єагв®.	{"Њ­®Ј® «Ё­пҐв",‹ бЄ®ў п,ЂЄвЁў­ п}	баҐ¤­пп
-Ђўбва «Ё©бЄЁ© вҐамҐа	https://catfishes.ru/wp-content/uploads/2020/03/malsob1.jpg	®Є®«® 6.5 ЄЈ	¤® 25 б¬	®в 12 ¤® 15 «Ґв	Companion	Ќг нв® Ў §®ўл© ЇаЁ¬Ґа ЄаЁ­¦®ў®© б®Ў ЄЁ. †Ё¤Є® ¤ бв Ї®¤ бҐЎп Ё б¤®е­Ґв. ЌҐ аҐЄ®¬Ґ­¤го	{"Њ «® «Ё­пҐв","ЋзҐ­м ЇаҐ¤ ­­ п",„аг¦Ґ«оЎ­ п,"‚лб®ЄЁ© Ё­вҐ««ҐЄв"}	¬Ґ«Є п
-ЂЄЁв -Ё­г	https://catfishes.ru/wp-content/uploads/2021/06/akita4.jpg	¤® 60 ЄЈ	¤® 71 б¬	®в 10 ¤® 13 «Ґв	Companion	џ ­Ґ б¬®ваҐ« • вЁЄ®, ­® нв  б®Ў Є , ўа®¤Ґ, ®ввг¤ . ‚лЈ«п¤Ёв ЇаЁЄ®«м­®, ¤  Ґйс Ё ўҐа­ п ¤®«¦­  Ўлвм	{"Џ®¤е®¤Ёв ¤«п ®еа ­л","‚лб®ЄЁ© Ё­вҐ««ҐЄв","Џа®¦Ёў ­ЁҐ ў­Ґ ¤®¬ ","Џ®¤е®¤Ёв ¤«п ®е®вл","ЋвбгвбвўгҐв згўбвў® бва е "}	ЄагЇ­ п
+COPY public.deeperpagedogs (breed, photo, life_duration, main_purpose, full_description, main_characteristics, size, weight_border1, weight_border2, height_female, height_male) FROM stdin;
+Ђўбва «Ё©бЄ п ®ўз аЄ 	https://placepic.ru/wp-content/uploads/2019/07/merle-dog-long.jpg	 13 - 15 «Ґв	Companion	ЌҐ б®Ў Є  - ¬Ґзв 	{"ЋзҐ­м ЇаҐ¤ ­­ п",„аг¦Ґ«оЎ­ п,"Џ®¤е®¤Ёв ¤«п ®е®вл"}	ЄагЇ­ п	26	50	53	58
+ЂЄЁв -Ё­г	https://lapkins.ru/upload/uf/78e/78e8b448a6ebc51f00d4f68203c82c9b.jpg	 10 - 13 «Ґв	Companion	џЇ®­бЄЁ© Ў «¤с¦, ўбҐ¬ аҐЄ®¬Ґ­¤го	{"‚лб®ЄЁ© Ё­вҐ««ҐЄв","Џа®¦Ёў ­ЁҐ ў­Ґ ¤®¬ ","Џ®¤е®¤Ёв ¤«п ®е®вл"}	ЄагЇ­ п	35	50	66	71
+Џг¤Ґ«м	https://lapkins.ru/upload/iblock/6e5/6e52cb986e51a236498ecb062598d8a9.jpg	 10 - 18 «Ґв	Hunting	Љг¤апўҐ­мЄ п, ўлЈ«п¤Ёв § Ў ў­®	{"Њ «® «Ё­пҐв",„аг¦Ґ«оЎ­ п,"Џ®¤е®¤Ёв ¤«п ®е®вл","•®а®иҐҐ Ї®б«ги ­ЁҐ"}	ЄагЇ­ п	26	35	53	58
+ЌҐ¬ҐжЄ п ®ўз аЄ 	https://proprikol.ru/wp-content/uploads/2020/10/kartinki-nemeczkoj-ovcharki-1.jpg	 12 - 15 «Ґв	Service	‚®§¬®¦­® Ё¬Ґ­­® ®­  б­Ё¬ « бм ў Њгев аҐ	{"Џа®¦Ёў ­ЁҐ ў­Ґ ¤®¬ ","ЋзҐ­м ЇаҐ¤ ­­ п","Џ®¤е®¤Ёв ¤«п ®е®вл","•®а®иҐҐ Ї®б«ги ­ЁҐ"}	ЄагЇ­ п	28	40	60	65
+ЏҐЄЁ­Ґб	https://sobakevi4.ru/wp-content/uploads/2020/08/1.-pekines-imeet-drevnie-korni.jpg	 15 - 16 «Ґв	Decorative	‹Ёж® б¬Ґи­®Ґ	{"Њ «® « Ґв","ЋвбгвбвўгҐв згўбвў® бва е ","ЋзҐ­м ЇаҐ¤ ­­ п"}	¬Ґ«Є п	6	10	53	58
+— г-— г	https://porodysobak.com/wp-content/uploads/2019/12/chow-chow_01_lg.jpg	 9 - 15 «Ґв	Companion	ЋЈа®¬­ п Ё « бЄ®ў п	{"Џ®¤е®¤Ёв ¤«п ®еа ­л","ЋзҐ­м ЇаҐ¤ ­­ п"}	ЄагЇ­ п	25	32	51	56
+Њ «мвЁЇг	https://skstoit.ru/wp-content/uploads/2022/01/skolko-stoit-maltipu-1.jpg	 13 - 15 «Ґв	Decorative	Њ «Ґ­мЄ п, г¤®Ў­® Ўа вм б б®Ў®© ў ЄагЈ®бўҐв­®Ґ ЇгвҐиҐбвўЁҐ	{"Њ «® « Ґв","ЋзҐ­м ЇаҐ¤ ­­ п"}	¬Ґ«Є п	5	10	29	30
+Sharpey	https://cat4you.ru/wp-content/uploads/b/5/b/b5bbf97fa1462f2353a229592ec0e7ea.jpeg	 9 - 11 «Ґв	Companion	Џ®е®¦  ­  ЊЁи«Ґ­	{"‚лб®ЄЁ© Ё­вҐ««ҐЄв","Џ®¤е®¤Ёв ¤«п ®еа ­л","ЋзҐ­м ЇаҐ¤ ­­ п"}	баҐ¤­пп	11	25	50	52
 \.
 
 
@@ -277,9 +290,12 @@ COPY public.photogallery (breed, photos) FROM stdin;
 --
 
 COPY public.purposescounter (purpose, number_of) FROM stdin;
-Hunting	1
-Companion	2
 Pastoral	0
+Fighting	0
+Hunting	1
+Service	1
+Companion	4
+Decorative	2
 \.
 
 
@@ -287,7 +303,7 @@ Pastoral	0
 -- Data for Name: shelterdogs; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.shelterdogs (id, name, breed, age, vaccinations, shelter_name, gender, full_description, weight) FROM stdin;
+COPY public.shelterdogs (id, name, breed, age, vaccinations, shelter_name, gender, full_description, weight, height) FROM stdin;
 \.
 
 
@@ -312,7 +328,7 @@ COPY public.users (nickname, email, password, city) FROM stdin;
 -- Name: shelterdogs_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.shelterdogs_id_seq', 5, true);
+SELECT pg_catalog.setval('public.shelterdogs_id_seq', 8, true);
 
 
 --
