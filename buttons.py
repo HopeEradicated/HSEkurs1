9f5a0aa1378179b4511a3711a53c8e0058ba5481
Hrencgore <84382500+Hrencgore@users.noreply.github.com>
2022-06-12 17:43:37 +0300

    Bot v0.4
    Final version of Telegram Bot

---
+++ buttons.py
@@ -0,0 +1,14 @@
+from aiogram.types import KeyboardButton
+
+button_start = KeyboardButton('Запуск')
+button_help = KeyboardButton('Справка')
+button_search = KeyboardButton('Привет! 👋')
+button_match = KeyboardButton('Привет! 👋')
+button_matchhelp = KeyboardButton('Привет! 👋')
+button_shelterdogs = KeyboardButton('Привет! 👋')
+button_shelters = KeyboardButton('Привет! 👋')
+button_shelterinfo = KeyboardButton('Привет! 👋')
+button_link = KeyboardButton('Привет! 👋')
+button_select = KeyboardButton('Привет! 👋')
+button_selecthelp = KeyboardButton('Привет! 👋')
+button_photos = KeyboardButton('Привет! 👋')
